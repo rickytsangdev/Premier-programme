3b523be204cb6bbcc049f6a40101eb8504de28ad
rickytsangdev <rickytsang.dev@gmail.com>
2024-06-29 17:26:45 +0200

    les fonctions anonymes

---
+++ fonctons_anonymes.js
@@ -0,0 +1,15 @@
+// let fonctionAnonyme = function () {
+// 	console.log("je suis une fonction anonyme");
+// };
+
+// fonctionAnonyme();
+
+// une fonction anonyme peut être une fonction contenu dans une variable
+
+// une fonction anonyme peut être une fonction qui s'autoexecute
+
+// une fonction anonyme a de grandes utilisés dans la programmation orientée objet
+
+(function () {
+	console.log("je suis une fonction anonyme qui sexecute");
+})();
