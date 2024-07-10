c837ce2d78adc8763e6bce3513f41d5a61f83bfa
rickytsangdev <rickytsang.dev@gmail.com>
2024-07-10 22:29:27 +0200

    cassez la boucle avec break et continue

--- 19_notion_base_boucle_important.js
+++ 19_notion_base_boucle_important.js
@@ -35,3 +35,41 @@ for (let i = 1; i < 5; i++) {
 // Il s'agit de boucles qui vont être utilisées pour parcourir des objets. Nous n'avons pas encore vu cette notion, nous parlerons donc de ces boucles lorsque nous aborderons les objets, don't panic !
 
 // Je vous attends dans la prochaine session, on va casser nos boucles !
+
+// CASSER LA BOUCLE
+
+let n = 0;
+
+// le mot clé break sert à arrêté
+
+while (n < 5) {
+	console.log("Ligne : " + n);
+	n++;
+}
+
+let r = 0;
+
+// le mot clé break sert à arrêté la condition // elle bloque concretement !
+
+while (r < 5) {
+	if (r == 3) {
+		break;
+	}
+
+	console.log("la boucle s'arrete a " + r);
+	r++;
+}
+
+// le mot clé continue sert à passer immédiatement la prochaine itération !
+
+let t = 0;
+
+while (t < 5) {
+	if (t == 3) {
+		t++;
+		continue;
+	}
+
+	console.log("la boucle s'arrete a " + t);
+	t++;
+}
